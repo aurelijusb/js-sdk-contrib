bb4110d7a4ae9ee794163252d8ce35bcc3905813
Aurelijus Banelis <aurelijus@banelis.lt>
2026-03-15 19:52:27 +0200

    Include more fields for flagMetadata
    Using CamelCase and string casting to keep it easy to maintain, but not bloat too much the internals.
    
    ruleId - useful to debug which feature rule was exectued.
    source - similar to rule id, but more generic  (e.g. force vs experiment)
    experimentName, experimentResultName - useful in reporting tracking data.
    experimentSeed - used to have multiple experiemnts to evaluate similarly.
    experimentKey - used for tracking key
    experimentPhase - used after reshufling

--- libs/providers/growthbook/src/lib/growthbook-provider.spec.ts
+++ libs/providers/growthbook/src/lib/growthbook-provider.spec.ts
@@ -84,7 +84,11 @@ describe('GrowthbookProvider', () => {
       const res = await ofClient.getBooleanDetails(testFlagKey, false);
       expect(res).toEqual({
         flagKey: testFlagKey,
-        flagMetadata: {},
+        flagMetadata: {
+          ruleId: 'test',
+          source: 'experiment',
+          experimentResultHashValue: 'abc',
+        },
         value: true,
         reason: 'experiment',
         variant: 'treatment',
@@ -114,7 +118,11 @@ describe('GrowthbookProvider', () => {
       const res = await ofClient.getStringDetails(testFlagKey, '');
       expect(res).toEqual({
         flagKey: testFlagKey,
-        flagMetadata: {},
+        flagMetadata: {
+          ruleId: 'test',
+          source: 'experiment',
+          experimentResultHashValue: 'abc',
+        },
         value: 'Experiment fearlessly, deliver confidently',
         reason: 'experiment',
         variant: 'treatment',
@@ -144,7 +152,11 @@ describe('GrowthbookProvider', () => {
       const res = await ofClient.getNumberDetails(testFlagKey, 1);
       expect(res).toEqual({
         flagKey: testFlagKey,
-        flagMetadata: {},
+        flagMetadata: {
+          ruleId: 'test',
+          source: 'experiment',
+          experimentResultHashValue: 'abc',
+        },
         value: 12345,
         reason: 'experiment',
         variant: 'treatment',
@@ -174,11 +186,109 @@ describe('GrowthbookProvider', () => {
       const res = await ofClient.getObjectDetails(testFlagKey, {});
       expect(res).toEqual({
         flagKey: testFlagKey,
-        flagMetadata: {},
+        flagMetadata: {
+          ruleId: 'test',
+          source: 'experiment',
+          experimentResultHashValue: 'abc',
+        },
         value: { test: true },
         reason: 'experiment',
         variant: 'treatment',
       });
     });
   });
+
+  describe('details resolvers', () => {
+    it('should include advanced experiment metadata', async () => {
+      jest.spyOn(GrowthBookClient.prototype, 'evalFeature').mockImplementation(() => ({
+        value: true,
+        source: 'experiment',
+        on: true,
+        off: false,
+        ruleId: 'fr_a4obdoommrzcapr',
+        experiment: {
+          key: 'tracking-key',
+          variations: [false, true],
+          name: 'Experiment1',
+          phase: '0',
+          seed: 'e29f8131-b6ba-4a39-b9cd-e34d4bb74b33',
+        },
+        experimentResult: {
+          value: true,
+          variationId: 1,
+          key: '1',
+          name: 'Variation 1',
+          inExperiment: true,
+          hashAttribute: 'id',
+          hashValue: '1',
+          featureId: 'feature1',
+        },
+      }));
+
+      const res = await ofClient.getBooleanDetails('feature1', false);
+
+      expect(res).toEqual({
+        flagKey: 'feature1',
+        flagMetadata: {
+          ruleId: 'fr_a4obdoommrzcapr',
+          source: 'experiment',
+          experimentName: 'Experiment1',
+          experimentKey: 'tracking-key',
+          experimentPhase: '0',
+          experimentSeed: 'e29f8131-b6ba-4a39-b9cd-e34d4bb74b33',
+          experimentResultName: 'Variation 1',
+          experimentResultHashValue: '1',
+        },
+        value: true,
+        reason: 'experiment',
+        variant: '1',
+      });
+    });
+
+    it('expose in meta experiment reshuffle', async () => {
+      jest.spyOn(GrowthBookClient.prototype, 'evalFeature').mockImplementation(() => ({
+        value: true,
+        source: 'experiment',
+        on: true,
+        off: false,
+        ruleId: 'fr_a4obdoommrzcapr',
+        experiment: {
+          key: 'tracking-key',
+          variations: [false, true],
+          name: 'Experiment1',
+          phase: '1',
+          seed: '9348fc34-519e-424d-b3ac-90593004808a',
+        },
+        experimentResult: {
+          value: true,
+          variationId: 1,
+          key: 'id2',
+          name: 'variatonName2',
+          inExperiment: true,
+          hashAttribute: 'id',
+          hashValue: '1',
+          featureId: 'feature1',
+        },
+      }));
+
+      const res = await ofClient.getBooleanDetails('feature1', false);
+
+      expect(res).toEqual({
+        flagKey: 'feature1',
+        flagMetadata: {
+          ruleId: 'fr_a4obdoommrzcapr',
+          source: 'experiment',
+          experimentName: 'Experiment1',
+          experimentKey: 'tracking-key',
+          experimentPhase: '1',
+          experimentSeed: '9348fc34-519e-424d-b3ac-90593004808a',
+          experimentResultName: 'variatonName2',
+          experimentResultHashValue: '1',
+        },
+        value: true,
+        reason: 'experiment',
+        variant: 'id2',
+      });
+    });
+  })
 });
